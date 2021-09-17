613a4b097020235ddc3d091294b7e08afa0acd6e
nazhinO <nazhinO@yahoo.com>
2021-09-17 01:29:39 +0000

    Removed general xampp/lampp files and thinned down the new paths Part 0

--- Vlog/vlog/admin/session.php
+++
@@ -1,9 +0,0 @@
-<?php
-   session_start();
-   if(isset($_SESSION['username'])){
-    header("localtion:dashboard.php");    
-   }
-   else{
-    header("location:index.php");
-   }
-?>
---
+++ sigin.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+include('connection.php');
+$username = $_POST['username'];
+$password = $_POST['password'];
+$password_encrypted =sha1($password);
+
+$sql = mysqli_query($con, "SELECT count(*) as total from user WHERE username = '".$username."' and password = '".$password_encrypted."'");
+$row = mysqli_fetch_array($sql);
+
+if($row["total"] > 0){
+    $_SESSION['username'] = $username;
+    echo "<script> alert('Login Success');window.location='../admin/dashboard.php'</script>"; 
+}else{
+    echo "<script> alert('Login Failed');window.location='../admin/index.php '</script>";
+}
+
+
+
+?>
